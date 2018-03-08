658a5a3803055bfa9d8c6e32c3334ecdbb1b14b5
Noah Kellem <nmk2485@g.rit.edu>
2018-03-07 21:56:28 -0500

    Started project

---
+++ client/js/index.js
@@ -0,0 +1,59 @@
+let canvas, ctx, user, users = {}, lastRender = 0;
+
+const draw = () => {
+  ctx.clearRect(0,0,1000,700);
+  
+  Object.keys(users).forEach(square => {
+    ctx.fillStyle = 'black';
+    
+    if (square === user) {
+      ctx.fillStyle = 'red';
+    }
+    
+    ctx.fillRect(users[square].x, users[square].y, users[square].width, users[square].height);
+  });
+};
+
+const handleInput = () => {
+  canvas.addEventListener('keydown', e => {
+    if (e.keyCode === 37) {
+      users[user].x -= 1;
+      draw();
+    }
+    
+    if (e.keyCode === 39) {
+      users[user].x += 1;
+      draw();
+    }
+  });
+};
+
+const init = () => {
+  const socket = io.connect();
+  canvas = document.querySelector('#canvas');
+  ctx = canvas.getContext('2d');
+  
+  user = `user${Math.floor(Math.random() * 1000)}`;
+  
+  users[user] = {
+    width: 100,
+    height: 100,
+    x: 450,
+    y: 0,
+  };
+  
+  let squareData = {user, width: 100, height: 100, x: 450, y: 0};
+  
+  socket.on('connect', () => {
+    socket.emit('join', squareData);
+  });
+  
+  socket.on('sendUsers', data => {
+    users = data;
+  });
+  
+  handleInput();
+  draw();
+}
+
+window.onload = init;
---
+++ src/server.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const socketio = require('socket.io');
+const nodeStatic = require('node-static');
+// instantiates the node-static object to allow for file serving
+const fileServer = new nodeStatic.Server(`${__dirname}/../client`, {
+  cache: false,
+  gzip: true,
+});
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  fileServer.serve(request, response, err => {
+    if (err) {
+      response.writeHead(404, { 'Content-Type': 'application.json' });
+      response.write('test', {
+        message: 'The page you are looking for was not found.',
+        id: 'notFound',
+      });
+      response.end();
+    }
+  });
+};
+
+const app = http.createServer(onRequest).listen(PORT);
+const io = socketio(app);
+
+console.log(`Listening on localhost:${PORT}`);
+
+const users = {};
+
+const onJoined = sock => {
+  const socket = sock;
+  
+  socket.on('join', data => {
+    socket.join('room1');
+    socket.name = data.user;
+    users[data.user] = {width: data.width, height: data.height, x: data.x, y: data.y};
+    socket.emit('sendUsers', users);
+    //socket.broadcast.emit('playerJoined', data);
+  });
+};
+
+const onUserJump = sock => {
+  const socket = sock;
+  
+  socket.on('jump')
+};
+
+const onDisconnect = sock => {
+  const socket = sock;
+  
+  socket.on('disconnect', () => {
+    socket.leave('room1');
+    delete users[socket.name];
+  });
+};
+
+io.sockets.on('connection', socket => {
+  console.log('started');
+  
+  onJoined(socket);
+  onDisconnect(socket);
+});
+
+console.log('Websockets server started');
